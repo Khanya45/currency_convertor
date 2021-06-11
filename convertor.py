05f7c4e1e049f1b45affe1c539b3547d4e771750
khanya <82158699+Khanya45@users.noreply.github.com>
2021-06-11 16:16:56 +0200

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -5,42 +5,48 @@ import getpass
 
 
 root = Tk()
-root.title("Requests")
-root.geometry("400x400")
+root.title("CURRENCY CONVERTOR")
+root.geometry("400x450")
+
+response = requests.get('https://v6.exchangerate-api.com/v6/fac9f0aa288dff6d0a7c7a88/latest/USD')
+text = response.json()
+conversion_rates = text['conversion_rates'].keys()
+
+def Clear():
+    edtCurs.delete(0, END)
+    edtUSD.delete(0, END)
 
 
 def request():
-    response = requests.get('https://v6.exchangerate-api.com/v6/fac9f0aa288dff6d0a7c7a88/latest/USD')
-    text = response.json()
-    print(text)
-    conversion_rates = text['conversion_rates'].keys()
-    CurList = Listbox(root)
-    CurList.place(x=170, y=120)
-    for i in conversion_rates:
-        CurList.insert(END, str(i))
-
-    dict_Currency = float(edtCurs.get()) * text['conversion_rates'][CurList.get(ACTIVE)]
-    answer = float(dict_Currency) * float(edtCurs.get())
-    print(answer)
-    # for i in dict_Currency.keys():
-    #     if i == edtCurrency.get():
-    #         messagebox.showinfo("working", "its working")
-    #val = edtCurrency.get().upper()
-    # dict_Currency = text['conversion_rates'][val]
+    if edtUSD.get() != "":
+        messagebox.showwarning("warning", "Clear the edits first")
+    else:
+        dict_Currency = float(edtCurs.get()) * text['conversion_rates'][CurList.get(ACTIVE)]
+        edtUSD.insert(0, dict_Currency)
 
 
-lblTitle = Label(root, text="choose the currency")
-lblTitle.place(x=55, y=80)
+lblTitle = Label(root, text="USD")
+lblTitle.place(x=80, y=80)
 edtUSD = Entry(root, width=10)
-edtUSD.place(x=40, y=100)
+edtUSD.place(x=70, y=100)
 
-lblCurs = Label(root, text="USD")
-lblCurs.place(x=185, y=80)
+lbCurrency = Label(root, text="choose a currency")
+lbCurrency.place(x=100, y=140)
+
+CurList = Listbox(root)
+CurList.place(x=100, y=180)
+for i in conversion_rates:
+    CurList.insert(END, str(i))
+
+lblCurs = Label(root, text="Enter amount")
+lblCurs.place(x=200, y=80)
 edtCurs = Entry(root, width=10)
-edtCurs.place(x=170, y=100)
-#
+edtCurs.place(x=200, y=100)
+
 btn1 = Button(root, text="CONVERT", command=request)
-btn1.place(x=105, y=300)
-#
+btn1.place(x=105, y=380)
+
+btnClear = Button(root, text="CLEAR", command=Clear)
+btnClear.place(x=200, y=380)
 
 root.mainloop()
